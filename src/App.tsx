00c148fe915361ca3d47b6acbd09a2502d3d5a4a
Shashank Acharya <acharya.s@icloud.com>
2022-11-29 23:14:37 +0800

    Change colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Card, Center } from '@mantine/core';
+import { Card, Center, useMantineTheme } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
 
 function App() {
+  const theme  = useMantineTheme()
   return (
-    <Center style={{ minHeight: '100vh', backgroundColor: '#c0392b' }} p='sm'>
+    <Center style={{ minHeight: '100vh', backgroundColor: theme.colors.red[8] }} p='sm'>
       <Card shadow='sm' radius='md' style={{ width: '100%', maxWidth: '600px' }} p='xl'>
         <Outlet/>
       </Card>
