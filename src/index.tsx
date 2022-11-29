0161f3c9433aa4e9d2ef0ce6e245c5252544d245
Shashank Acharya <acharya.s@icloud.com>
2022-11-29 22:01:43 +0800

    Add react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import { MantineProvider } from '@mantine/core';
+import { createBrowserHistory } from '@remix-run/router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const mainRouter = createBrowserRouter([
+  { path: '/', element: <App/>}
+])
+
 root.render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <App/>
+      <RouterProvider router={mainRouter}/>
     </MantineProvider>
   </React.StrictMode>
 );
