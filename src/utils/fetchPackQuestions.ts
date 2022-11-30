b87a9a3169f372612f2c6611f49bcc482458cdf6
Shashank Acharya <acharya.s@icloud.com>
2022-11-30 13:32:08 +0800

    Change fetch location to master

--- src/utils/fetchPackQuestions.ts
+++ src/utils/fetchPackQuestions.ts
@@ -1,5 +1,5 @@
 export default async function fetchPackQuestions(pack: string | undefined) {
-	return fetch(`https://raw.githubusercontent.com/sheshenk/waes/branch-InitLayout/src/assets/packs/${pack}.json`)
+	return fetch(`https://raw.githubusercontent.com/sheshenk/waes/master/src/assets/packs/${pack}.json`)
 		.then(res => res.json())
 		.then(res => res.questions ? res.questions : [])
 }
