61d4235b0251bd7684df27bd2f1ed7344724fde2
Shashank Acharya <acharya.s@icloud.com>
2022-12-01 13:11:25 +0800

    Implement seeded shuffle

--- src/utils/filterQuestions.ts
+++ src/utils/filterQuestions.ts
@@ -1,9 +1,27 @@
 export default function filterQuestions(allQuestions: Question[], seed: string | undefined) {
+	const seedNum = seed ? parseInt(seed) : 0
 	const [levelOne, levelTwo, levelThree] = [1,2,3].map(n => {
 		const sampled = allQuestions.filter(q => q.level === n)
-		const shuffled = sampled // implement random shuffling here
+		const shuffled = shuffle(sampled, seedNum)
 		const sliced = shuffled.slice(0, Math.min(shuffled.length, 10))
 		return sliced.map((q, i) => ({...q, qnum: i + 1}))
 	})
 	return [...levelOne, ...levelTwo, ...levelThree]
+}
+
+function random(seed: number) {
+	var x = Math.sin(seed++) * 10000
+	return x - Math.floor(x)
+}
+
+function shuffle(array: Question[], seed: number) {
+	var m = array.length, t, i
+	while (m) {
+		i = Math.floor(random(seed) * m--)
+		t = array[m]
+		array[m] = array[i]
+		array[i] = t
+		++seed
+	}
+	return array
 }
