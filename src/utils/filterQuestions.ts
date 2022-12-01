ea5b8f8f7937e423e56edf188f7eed3e212e924c
Shashank Acharya <acharya.s@icloud.com>
2022-12-01 10:15:59 +0800

    Filter questions without random sampling

--- src/utils/filterQuestions.ts
+++ src/utils/filterQuestions.ts
@@ -1,3 +1,9 @@
 export default function filterQuestions(allQuestions: Question[], seed: string | undefined) {
-	return allQuestions
+	const [levelOne, levelTwo, levelThree] = [1,2,3].map(n => {
+		const sampled = allQuestions.filter(q => q.level === n)
+		const shuffled = sampled // implement random shuffling here
+		const sliced = shuffled.slice(0, Math.min(shuffled.length, 10))
+		return sliced.map((q, i) => ({...q, qnum: i + 1}))
+	})
+	return [...levelOne, ...levelTwo, ...levelThree]
 }
